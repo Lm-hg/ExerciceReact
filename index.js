51f89cfc9eaebbb5d87aa18e088cb2f52b73217d
Lm-hg <you@example.com>
2024-05-30 10:36:29 +0200

    stylisation formulaire d'ajout

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.post('/', async(req,res)=>{
-    const data = req.body;
 
+    const data = req.body;
+    if(data.nom !='' && data.prix!= '' && data.details!='' && data.url !=''){
         res.status(200).json(data);
+    }else{
+        res.status(400).json({message:'Champ vide'});
+    }
+
    
     
 })
--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -4,15 +4,27 @@ export default function Form(props){
         <div className="page">
 
             <div className="parent">
-                <span onClick={props.close}>&times;</span>
                 <form action="">
-                    <input type="text" name="" id="nom" placeholder="Nom"/>
+                     <span onClick={props.close}>&times;</span>
+                    <div className="a">
+                        <label htmlFor="">Nom d'article :</label>
+                        <input type="text" name="" id="nom" placeholder="Nom"/>
+                    </div>
+                    <div className="a">
+                        <label htmlFor="">Le prix :</label>
                     <input type="number" name="" id="prix" placeholder="Prix"/>
-                    <input type="text" name="" id="details" placeholder="Details"/>
-                    <input type="url" name="lien" id="Lien" />
+                    </div>
+                    <div className="a">
+                        <label htmlFor="">Description :</label>
+                      <input type="text" name="" id="details" placeholder="Details"/>
+                    </div>
+                    <div className="a">
+                        <label htmlFor="">Le lien d'image :</label>
+                        <input type="url" name="lien" id="Lien" />
+                    </div>
                     <div className="bouton">
 
-                        <input type="submit" value="ADD" onClick={props.envoyer}/>
+                        <input className="btn outline" type="submit" value="ADD" onClick={props.envoyer}/>
                     </div>
 
                 </form>
